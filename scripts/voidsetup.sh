c50d99d8b1ae7a389dea3d53417595cf1dd29138
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-07-20 09:24:27 +0900

    Update voidsetup.sh

--- scripts/voidsetup.sh
+++ scripts/voidsetup.sh
@@ -1,27 +1,28 @@
 #!/bin/sh
 sudo xbps-install -u xbps
 sudo xbps-install -Syu
-sudo xbps-install -Sy void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
+sudo xbps-install -Rsy void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
 
-sudo xbps-install -Sy vim neovim htop fastfetch ufw gufw gvfs xdg-user-dirs-gtk stow
-sudo xbps-install -Sy wayland xorg-server-xwayland
-sudo xbps-install -Sy dbus elogind polkit
-sudo xbps-install -Sy xf86-video-intel mesa-dri intel-ucode
-sudo xbps-install -Sy openh264 ffmpeg
-sudo xbps-install -Sy NetworkManager network-manager-applet
-sudo xbps-install -Sy pipewire wireplumber pipewire-pulse pavucontrol
-sudo xbps-install -Sy noto-fonts-cjk noto-fonts-emoji font-adobe-source-code-pro
-sudo xbps-install -Sy fcitx5 fcitx5-mozc fcitx5-configtool fcitx5-gtk+2 fcitx5-gtk+3 fcitx5-gtk4 fcitx5-qt5 fcitx5-qt6
-sudo xbps-install -Sy bluez blueman
+sudo xbps-install -Sy curl wget git vim neovim unzip zip nano gparted xtools ntfs-3g fuse-exfat bash-completion linux-headers gcc htop fastfetch ufw gufw gvfs xdg-user-dirs-gtk stow
+sudo xbps-install -y wayland xorg-server-xwayland
+sudo xbps-install -y dbus elogind polkit seatd
+sudo xbps-install -y xf86-video-intel mesa-dri mesa-intel-dri intel-ucode
+sudo xbps-install -y openh264 ffmpeg
+sudo xbps-install -y NetworkManager NetworkManager-openconnect NetworkManager-vpnc NetworkManager-l2tp network-manager-applet
+sudo xbps-install -y pipewire wireplumber pavucontrol
+sudo xbps-install -y noto-fonts-cjk noto-fonts-ttf-extra noto-fonts-emoji font-adobe-source-code-pro font-awesome font-fira-ttf
+sudo xbps-install -y fcitx5 fcitx5-mozc fcitx5-configtool fcitx5-gtk+2 fcitx5-gtk+3 fcitx5-gtk4 fcitx5-qt5 fcitx5-qt6
+sudo xbps-install -y bluez blueman
+sudo xbps-install -y flatpak
 
 sudo ln -snv /etc/sv/dbus /var/service/
-sudo ln -snv /etc/sv/elogind /var/service/
 sudo ln -snv /etc/sv/NetworkManager /var/service/
-#sudo ln -snv /etc/sv/pipewire /var/service/
-#sudo ln -snv /etc/sv/wireplumber /var/service/
+sudo ln -snv /etc/sv/bluetoothd /var/service/
+sudo unlink /var/service/dhcpcd
 
-sudo xbps-install -Sy firefox
-sudo xbps-install -Sy foot kitty alacritty mako wl-clipboard dolphin
-sudo xbps-install -Sy Thunar thunar-archive-plugin zip unzip p7zip
+sudo xbps-install -y firefox
+sudo xbps-install -y foot kitty alacritty dunst wl-clipboard dolphin
+sudo xbps-install -y Thunar thunar-archive-plugin
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 echo 'Completed.'
